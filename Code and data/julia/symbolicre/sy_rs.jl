43dab35f4d777a7ada2d11e280eafa48a32b6bd8
SChaoDDD <2678278542@qq.com>
2026-04-09 15:32:21 +0800

    Add files via upload

--- Code and data/julia/symbolicre/sy_rs.jl
+++ Code and data/julia/symbolicre/sy_rs.jl
@@ -4,11 +4,11 @@ using Zygote
 using MLJBase: machine, fit!, predict, report
 using Test
 using CSV
-data = CSV.File("C:/Users/XLYC/Desktop/ILbeta.csv")
+data = CSV.File("C:/Users/XLYC/Desktop/流感代码修改/R/SR/total_34nb.csv")
 
-
-col1 = data.temp  
-col2 = data.lag7  
+# 提取两列并转换为数值向量
+col1 = data.temp  # 提取第一列
+col2 = data.RR  # 提取第二列
 x = Vector{Float64}(col1)
 x= reshape(x, length(x), 1) 
 y = Vector{Float64}(col2)
@@ -16,7 +16,7 @@ y = Vector{Float64}(col2)
 model = SRRegressor(
     binary_operators=[+, -, *, /],
     unary_operators=[exp],
-    niterations=30
+    niterations=100
 )
 mach = machine(model, x, y)
 fit!(mach)
